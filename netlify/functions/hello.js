5ec1ff914faaab9ab6aaa9c584a4dc2c9b66688e
Claros <gimai0101@gmail.com>
2024-08-22 17:45:37 +0900

    Update

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,13 +1,32 @@
+const axios = require('axios');
+
 const handler = async (event) => {
   try {
-    const name = event.queryStringParameters.name ?? 'World'
+    const apiUrl = 'https://www.jalan.net/activity/json/arealist.json';
+    const response = await axios.get(apiUrl);
+    
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${name}!` }),
-    }
+      body: JSON.stringify(response.data),
+    };
   } catch (error) {
-    return { statusCode: 500, body: error.toString() }
+    // エラーがHTTPレスポンスに関連する場合
+    if (error.response) {
+      return {
+        statusCode: error.response.status,
+        body: JSON.stringify({ 
+          message: error.response.statusText, 
+          data: error.response.data 
+        }),
+      };
+    }
+    
+    // その他のエラー
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: error.message }),
+    };
   }
-}
+};
 
-module.exports = { handler }
+module.exports = { handler };
